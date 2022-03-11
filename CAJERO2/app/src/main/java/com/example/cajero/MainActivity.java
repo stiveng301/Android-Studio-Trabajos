fccac26709723fdd3626fc2daeeb015d02390418
stiveng301 <brayan.sti70@gmail.com>
2022-03-10 23:33:42 -0500

    Entrega 1

--- CAJERO2/app/src/main/java/com/example/cajero/MainActivity.java
+++ CAJERO2/app/src/main/java/com/example/cajero/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,22 +18,22 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText cedula, nombre, saldo, debitar;
     TextView resultado;
-    Float saldoini, saldodebitado;
+    float saldoini, saldodebitado, resta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cedula=(EditText) findViewById(R.id.etcedula);
-        nombre=(EditText) findViewById(R.id.etnombre);
-        saldo=(EditText) findViewById(R.id.etsaldo);
-        debitar=(EditText) findViewById(R.id.etdebisaldo);
+        cedula = (EditText) findViewById(R.id.etcedula);
+        nombre = (EditText) findViewById(R.id.etnombre);
+        saldo = (EditText) findViewById(R.id.etsaldo);
+        debitar = (EditText) findViewById(R.id.etdebisaldo);
+        resultado = (TextView) findViewById(R.id.resultado);
 
     }
 
-    public void registrar(View view)
-    {
+    public void registrar(View view) {
 
         AdminBD admin = new AdminBD(this, "basedatos", null, 1);
         //Abrir la base de datos (instanciar la base de datos)
@@ -52,14 +53,13 @@ public class MainActivity extends AppCompatActivity {
             }
         }*/
 
-       if (!Rcedula .isEmpty() && !Rnombre.isEmpty() && !Rsaldo.isEmpty())
-        {
+        if (!Rcedula.isEmpty() && !Rnombre.isEmpty() && !Rsaldo.isEmpty()) {
 
             ContentValues registro = new ContentValues();
             //Almacenar datos
-            registro.put("cedula",Rcedula);
-            registro.put("nombre",Rnombre);
-            registro.put("saldo",Rsaldo);
+            registro.put("cedula", Rcedula);
+            registro.put("nombre", Rnombre);
+            registro.put("saldo", Rsaldo);
             //Insertar la información en la tabla
             BaseDatos.insert("usuario", null, registro);
             //Cerrar Base de datos
@@ -69,10 +69,8 @@ public class MainActivity extends AppCompatActivity {
             nombre.setText("");
             saldo.setText("");
 
-            Toast.makeText(this,"Registro almacenado correctamente", Toast.LENGTH_LONG).show();
-        }
-        else
-        {
+            Toast.makeText(this, "Registro almacenado correctamente", Toast.LENGTH_LONG).show();
+        } else {
 
             Toast.makeText(this, "Ingrese correctamente todos los datos", Toast.LENGTH_LONG).show();
 
@@ -80,61 +78,62 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void consultar(View view)
-    {
+    public void consultar(View view) {
 
-        AdminBD admin = new AdminBD(this, "basedatos", null, 1 );
+        AdminBD admin = new AdminBD(this, "basedatos", null, 1);
         SQLiteDatabase BasedeDatos = admin.getWritableDatabase();
         String cedula1 = cedula.getText().toString();
 
-        if(!cedula1.isEmpty())
-        {
+        if (!cedula1.isEmpty()) {
 
-            Cursor fila = BasedeDatos.rawQuery("select nombre, saldo from usuario where cedula="+cedula1, null);
+            Cursor fila = BasedeDatos.rawQuery("select nombre, saldo from usuario where cedula=" + cedula1, null);
 
-            if(fila.moveToFirst())
-            {
+            if (fila.moveToFirst()) {
                 nombre.setText(fila.getString(0));
                 saldo.setText(fila.getString(1));
                 BasedeDatos.close();
                 Toast.makeText(this, "Datos consultados correctamente", Toast.LENGTH_SHORT).show();
-            }
-            else
-            {
+            } else {
                 Toast.makeText(this, "No se encontró el usuario", Toast.LENGTH_SHORT).show();
             }
-        }
-        else
-        {
+        } else {
             Toast.makeText(this, "Ingrese un documento de consulta", Toast.LENGTH_SHORT).show();
         }
 
     }
 
-    public void Retirar(View view)
-    {
+    public void Retirar(View view) {
 
-        float result;
+
         String saldoS = saldo.getText().toString();
         String saldodebitadoS = debitar.getText().toString();
 
         saldoini = Float.parseFloat(saldoS);
         saldodebitado = Float.parseFloat(saldodebitadoS);
 
-        if (!saldoS.isEmpty()){
 
+        if (!saldoS.isEmpty()) {
+
+            if (saldoini > saldodebitado) {
+
+                resta = (saldoini - saldodebitado);
+                Log.i("resta",String.valueOf(resta));
+
+                resultado.setText(String.valueOf(resta));
+                Toast.makeText(this, "Saldo debitado correctamente)"+ resta, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "No se puede hacer el retiro (Valor mayor al permitido)", Toast.LENGTH_SHORT).show();
+            }
+
+
+        } else {
             Toast.makeText(this, "Por favor ingrese un valor a debitar", Toast.LENGTH_SHORT).show();
-
-        }
-        else if (saldoini<saldodebitado){
-
-            result = (saldoini-saldodebitado);
-            resultado.setText(String.valueOf(result));
-            Toast.makeText(this, "Saldo debitado correctamente)", Toast.LENGTH_SHORT).show();
-        }else
-            Toast.makeText(this, "No se puede hacer el retiro (Valor mayor al permitido)", Toast.LENGTH_SHORT).show();
         }
 
     }
+}
+
+
+
 
 
