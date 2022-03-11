1ecb4536dd1bea21d92163d9c408c7533354b81a
stiveng301 <brayan.sti70@gmail.com>
2022-03-10 23:49:15 -0500

    Cajero

---
+++ Entrega_1/app/src/main/java/com/example/entrega_1/AdminBD.java
@@ -0,0 +1,24 @@
+package com.example.entrega_1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminBD extends SQLiteOpenHelper {
+    public AdminBD(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    //Creación de la base de datos
+    @Override
+    public void onCreate(SQLiteDatabase basedatos) {
+        basedatos.execSQL("create table usuario(cedula int primary key, nombre text, saldo int)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
