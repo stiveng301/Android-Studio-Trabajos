6b6f2b84e93a09d24863238c900b39fa97dd0607
stiveng301 <brayan.sti70@gmail.com>
2022-03-10 23:16:31 -0500

    Clase ciclo de vida

--- CilodeVida/app/src/main/java/com/example/cilodevida/MainActivity.java
+++ CilodeVida/app/src/main/java/com/example/cilodevida/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.cilodevida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String TAG = "Actividad Uno";
-    Button btn1;
+    Button HacerLlamada;
+    String NumALlamar = "3218035313";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart(){
         super.onStart();
+
+        HacerLlamada = findViewById(R.id.Llamar);
+        HacerLlamada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Hacerllamada(NumALlamar);
+
+            }
+
+        });
         Log.i(TAG, "--OnStart--");
     }
 
@@ -59,9 +72,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void SegundaPantalla(View view){
 
-        Intent IrVista2 = new Intent(MainActivity.this, MainActivity2.class);
+        Intent IrVista2 = new Intent(MainActivity.this, MainActivity2.class)
+                .putExtra("Email","brayan.sti70@gmail.com")
+                .putExtra("Clave","Sisas123");
         startActivity(IrVista2);
 
+    }
+
+    public void Hacerllamada (String Numerodetelefono){
+    Intent Llamada = new Intent(Intent.ACTION_DIAL);
+    Llamada.setData(Uri.parse("Tel: "+Numerodetelefono));
+        if(Llamada.resolveActivity(getPackageManager())!=null)
+        {
+            startActivity(Llamada);
+        }
     }
 
 }
--- CilodeVida/app/src/main/java/com/example/cilodevida/MainActivity2.java
+++ CilodeVida/app/src/main/java/com/example/cilodevida/MainActivity2.java
@@ -3,12 +3,23 @@ package com.example.cilodevida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    TextView TextEmail, TextPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextEmail = findViewById(R.id.Correo);
+        TextPass = findViewById(R.id.Clave);
+
+        String EMAIL = getIntent().getStringExtra("Email");
+        String PASS = getIntent().getStringExtra("Clave");
+
+        TextEmail.setText(EMAIL);
+        TextPass.setText(PASS);
     }
 }
